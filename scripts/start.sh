e748afab0aaeeb151f791e69e04d987c33cb773d
hwangseokjun <unbeatablesj@naver.com>
2022-05-24 03:36:14 +0900

    test3

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,7 +4,7 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh # 자바 import와 동일
 
-REPOSITORY=/home/ec2-user/app/step3
+REPOSITORY=/home/ubuntu/app/step3
 PROJECT_NAME=blogProject
 
 echo "> 빌드 파일 복사"
@@ -27,4 +27,4 @@ echo "> $JAR_NAME 실행"
 IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
-nohup java -jar -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties -Dspring.profiles.active=$IDLE_PROFILE $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &health.sh
+nohup java -jar -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ubuntu/app/application-real-db.properties -Dspring.profiles.active=$IDLE_PROFILE $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &health.sh
