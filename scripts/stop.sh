f9a520cf140b2acedfb5399f1d739c7a3cb7ddb7
hwangseokjun <unbeatablesj@naver.com>
2022-05-24 04:16:59 +0900

    test10

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 
-ABSPATH=$(readlink -f $0)
-ABSDIR=$(dirname $ABSPATH)
-source ${ABSDIR}/profile.sh # 자바 import와 동일
+# stop.sh
+# 서버 중단을 위한 스크립트
 
-#IDLE_PORT에서 쉬고 구동 중인 애플리케이션이 있다면 끈다.
+ABSPATH=$(readlink -f $0)
+# ABSDIR : 현재 stop.sh 파일 위치의 경로
+ABSDIR=$(dirname $ABSPATH)
+# import profile.sh
+source ${ABSDIR}/profile.sh
+
 IDLE_PORT=$(find_idle_port)
 
-echo "> $IDLE_PORT 에서 구동 중이 애플리케이션 pid 확인"
+echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
 IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
 
-if [ -z ${IDLE_PID} ]; then
-  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+if [ -z ${IDLE_PID} ]
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $IDLE_PID"
   kill -15 ${IDLE_PID}
